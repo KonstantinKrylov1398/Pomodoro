126963e045f2d14a5a0aadf6d43ece0b52274f48
Константин Крылов <crulov@icloud.com>
2022-11-12 11:01:21 +0300

    рефакторинг кода: изменение работы ф-ции: 20 строк

--- src/utils/timeRender.tsx
+++ src/utils/timeRender.tsx
@@ -1,4 +1,14 @@
 export function timeRender(intervalWork: number) {
+  console.log(String(intervalWork).endsWith("1"));
+  if (intervalWork < 60 && String(intervalWork).endsWith("1")) {
+    return `${intervalWork} секунды `;
+  }
+  if (intervalWork > 60 && String(intervalWork).endsWith("1")) {
+    return `${intervalWork} минуты `;
+  }
+  if (intervalWork > 3600 && String(intervalWork).endsWith("1")) {
+    return `${intervalWork} часа `;
+  }
   if (intervalWork < 60) {
     return `${intervalWork} секунд `;
   }
